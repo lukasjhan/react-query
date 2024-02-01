6c4619647e3026073c7728acccccaa40ba06fc1c
Lukas.J.Han <lukas.j.han@gmail.com>
2024-02-02 00:18:12 +0900

    feat: code for test react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, useQuery } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+function fetchData() {
+  return fetch('http://localhost:3000').then((res) => res.text());
+}
+
+function TestComponent() {
+  const { data, isFetching } = useQuery(
+    {
+      queryKey: ['number'],
+      queryFn: fetchData,
+      //refetchInterval: 1000,
+      staleTime: 30000, // 5 seconds stale time
+      gcTime: 10000, // 30 seconds cache time
+    },
+    queryClient
+  );
+
+  if (isFetching) return <div>Loading...</div>;
+
+  return <div>Data: {data}</div>;
+}
 
 function App() {
+  const [showComponent, setShowComponent] = React.useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setShowComponent(!showComponent)}>
+        Toggle Component
+      </button>
+      {showComponent && <TestComponent />}
     </div>
   );
 }
